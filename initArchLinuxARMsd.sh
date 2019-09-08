9616ed4c90554668e271ce1392000ba4e20c9e2c
Joffrey Janiec <jjaniec@student.42.fr>
2019-09-08 17:24:59 +0200

    add initArchLinuxARMsd.sh

---
+++ initArchLinuxARMsd.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#d Format sd card & burn arch linux arm image
+#u sudo ./initArchLinuxARMsd.sh /dev/sdX imagemodel (ex for rpi4: 4, see step 5 https://archlinuxarm.org/platforms/armv8/broadcom/raspberry-pi-4)
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o verbose
+#set -o xtrace
+
+OS=$(uname -s)
+
+if [ "${OS}" != "Linux" ];
+then
+ echo "OS != Linux"
+ exit 1
+fi;
+
+if [ -b ${1} ] && [ "${2}" != "" ];
+then
+ DIR=$(mktemp -d)
+ fdisk -l ${1}
+ cd ${DIR}
+ echo "Working dir ${PWD}, using device ${1} starting in 5 seconds"
+
+ wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-${2}-latest.tar.gz
+
+ fdisk ${1} <<EOF
+o
+p
+n
+p
+1
+
++100M
+t
+c
+n
+p
+2
+
+
+w
+EOF
+
+ mkfs.vfat ${1}1
+ mkfs.ext4 ${1}2
+ mkdir boot
+ mkdir root
+ mount ${1}1 boot
+ mount ${1}2 root
+
+ bsdtar -xpf ArchLinuxARM-rpi-${2}-latest.tar.gz -C root;
+ sync
+
+ mv root/boot/* boot
+
+ umount boot root
+ rm -rf ${DIR}
+ echo "Done"
+else
+ echo "Usage: ./initArchLinuxARMsd.sh /dev/sdX imagemodel (ex for rpi4: 4, see step 5 https://archlinuxarm.org/platforms/armv8/broadcom/raspberry-pi-4)"
+fi;
